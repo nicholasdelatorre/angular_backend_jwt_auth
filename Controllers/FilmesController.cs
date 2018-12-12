aa3c9e95f1e9719af50fca81de7d651dae2b5a87
Nicholas Dela Torre <nicholas.delatorre@gmail.com>
2018-12-12 11:57:04 -0200

    Primeiro commit

---
+++ Controllers/FilmesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class FilmesController : ControllerBase
+    {
+        [HttpGet, Authorize] // Authorize: Apenas será ezibido ser for autorizado
+        public IEnumerable<Filme> Listar()
+        {
+            return new Filme[] {
+                new Filme("Matrix", 1999,
+                    new Diretor[]{
+                        new Diretor("Irmão 1", "Wachowski"),
+                        new Diretor("Irmão 2", "Wachowski")
+                    }),
+                new Filme("Lagoa Azul", 1980),
+                new Filme("Os Incríveis 2", 2014)
+            };
+        }
+    }
+
+    public class Filme
+    {
+        public Filme(string titulo, int ano)
+        {
+            Titulo = titulo;
+            Ano = ano;
+            Diretores = new Diretor[] { };
+        }
+        public Filme(string titulo, int ano, Diretor[] diretores)
+        {
+            Titulo = titulo;
+            Ano = ano;
+            Diretores = diretores;
+        }
+
+        public string Titulo { get; private set; }
+        public int Ano { get; private set; }
+        public Diretor[] Diretores { get; private set; }
+    }
+
+    public class Diretor
+    {
+        public Diretor(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
+        public string Nome { get; private set; }
+        public string Sobrenome { get; private set; }
+    }
+}
---
+++ Controllers/TokenController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private IConfiguration _config;
+        public TokenController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult CreateToken([FromBody]LoginModel model)
+        {
+            IActionResult response = Unauthorized();
+
+            UserModel user = Authenticate(model);
+
+            if (user != null)
+            {
+                string tokenString = BuildToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+        private string BuildToken(UserModel user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              expires: DateTime.Now.AddMinutes(1),
+              signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel Authenticate(LoginModel model)
+        {
+            if (model.Login == "everis" && model.Password == "everis01")
+                return new UserModel
+                {
+                    Name = "Laerte",
+                    Email = "laerte.uliam@gmail.com",
+                    BirthDate = new DateTime(1983, 11, 26)
+                };
+
+            return null;
+        }
+    }
+
+}
